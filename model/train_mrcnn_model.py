8603d033ea638b99472f47b5fddac3d940c13d61
John Hoff <john.hoff@braindonor.net>
2020-02-16 20:28:31 -0600

    Updating with the new model.

--- model/train_mrcnn_model.py
+++ model/train_mrcnn_model.py
@@ -13,6 +13,9 @@ import matplotlib.pyplot as plt
 import glob
 import skimage
 
+from skimage.io import imread
+import pandas as pd
+
 from mrcnn.config import Config
 from mrcnn import utils
 import mrcnn.model as modellib
@@ -33,81 +36,38 @@ class BuildingConfig(Config):
     NUM_CLASSES = 1 + 1  # 1 background + 1 Building class
     IMAGE_MIN_DIM = 256
     IMAGE_MAX_DIM = 256
-    STEPS_PER_EPOCH = 4
-    VALIDATION_STEPS = 1
+    STEPS_PER_EPOCH = 100  # TODO: Find a clean way to override this.  It's just getting replaced in code for now
+    VALIDATION_STEPS = 10 # TODO: Find a clean way to override this.  It's just getting replaced in code for now
     MEAN_PIXEL = np.array([123.7, 116.8, 103.9]) # TODO: Update with better means
 
 
 class BuildingDataset(utils.Dataset):
+    def __init__(self, images_df):
+        super().__init__()
+        self.images_df = images_df
 
-    def load_building(self, dataset_dir, subset):
-        print("Loading buildings...")
-        """Load a subset of the building dataset.
-               dataset_dir: Root directory of the dataset.
-               subset: Subset to load: train or val
-               """
-        # This will need to be changed based on the images loaded
-        # dataset_dir = "/Users/Sebastian/Documents/GitHub/DSC672/"
-        # Add classes. We have only one class to add.
+    def load_building(self, validation=False):
+        """
+        Load the buildings from the dataframe of images and the validation flag
+        :param images_df:
+        :param validation:
+        :return:
+        """
         self.add_class("building", 1, "building")
-
-        # Which Dataset are you loading in?
-        # Val:Use validation test est
-        # train : use data from temp_data/sample/tile -256
-        # TO DO: ADD other training/testing sources
-        assert subset in ["sample_lg/", "val"]
-        subset_dir = "sample_lg/tile-256/"
-        dataset_dir = os.path.join(dataset_dir, subset_dir)
-        if subset == "val":
-            image_ids = VAL_IMAGE_IDS
-        else:
-            # Get image Ids from directory names
-            # print(dataset_dir) ## Remove latter DEBUG
-            image_ids = next(os.walk(dataset_dir))[2]
-            # image_ids = [f for f in listdir(dataset_dir) if isfile(join(dataset_dir, f))]
-            # print(image_ids) ## Remove Later DEBUG
-            if subset == "sample_lg/":
-                image_ids = list(set(image_ids) - set(VAL_IMAGE_IDS))
-
-        # dir_name = join(dataset_dir + "temp_data/sample/tile-256") # for loading in regualr tiles
-        # annotations_b = [f for f in listdir(dir_name) if isfile(join(dir_name, f))]
-
-        # dir_name = join(dataset_dir + "temp_data/sample/mask-256") # for loading in mask tiles
-        # annotations_m = [f for f in listdir(dir_name) if isfile(join(dir_name, f))]
-
-        # Add images
-        for image_id in image_ids:
-            print("Adding Image!")
-            print("building")
-            print(image_id)
-            print(os.path.join(dataset_dir, image_id.format(image_id)))
-
-            self.add_image(
-                "building",
-                image_id=image_id,
-                path=os.path.join(dataset_dir, image_id.format(image_id)))
-
-        print("DONE!")
+        for index, row in self.images_df.iterrows():
+            if (validation and row['validation'] == 1.0) or (not validation and row['validation'] == 0.0):
+                self.add_image("building", image_id=index, path=row['image'])
 
     def load_mask(self, image_id):
-        info = self.image_info[image_id]
-        # print("This is info ",str(info))
-        # Get mask directory from image path
-        mask_dir = os.path.join(os.path.dirname(os.path.dirname(info['path'])), "mask-256")
-        # print("this is ", mask_dir) # REMOVE LATER FOR DEBUG
-        # Read mask files from .png image
-        mask = []
-        # image_ids = [f for f in listdir(dataset_dir) if isfile(join(dataset_dir, f))]
-        for f in next(os.walk(mask_dir))[2]:
-            if f == info['id']:  # match image id to mask id
-                m = skimage.io.imread(os.path.join(mask_dir, f)).astype(np.bool)
-                mask.append(m)
-
-        # print("masks has {} items".format(len(mask)))
+        """
+        Return the mask for the selected image.
+        :param image_id:
+        :return:
+        """
+        # Grab the mask image using the pandas data frame and convert it into the needed format.
+        image = self.images_df.iloc[image_id]
+        mask = [imread(image['mask'])]
         mask = np.stack(mask, axis=-1)
-        # print("mask's shape is {}".format(mask.shape))
-        # Return mask, and array of class IDs of each instance. Since we have
-        # one class ID, we return an array of ones
         return mask, np.ones([mask.shape[-1]], dtype=np.uint8)
 
     def image_reference(self, image_id):
@@ -132,9 +92,48 @@ def get_ax(rows=1, cols=1, size=8):
     return ax
 
 
+def get_training_images_split(train_set, split):
+    """
+    Load a directory of training images and create a random split on them
+    :param train_set:
+    :param split:
+    :return: A pandas dataframe with image, mask, and validation
+    """
+
+    # Quickly grab the file glob and stick it into a numpy array.  Done in numpy first, so that we can
+    # rotate it before turning it into a data frame.
+    file_array = np.rot90(np.array([
+        [filename for filename in glob.iglob('temp_data/%s/tile-256/**/*.png' % train_set, recursive=True)],
+        [filename for filename in glob.iglob('temp_data/%s/mask-256/**/*.png' % train_set, recursive=True)],
+    ]))
+    training_df = pd.DataFrame(file_array, columns=['image', 'mask'])
+
+    # Create an array of zeros and then set a random split fo them to 1, then save this as our validation column
+    validation = np.zeros(len(training_df))
+    indices = np.arange(len(training_df))
+    random.shuffle(indices)
+    for i in indices[:int(len(training_df)*split)]:
+        validation[i] = 1.0
+    training_df['validation'] = validation
+
+    return training_df
+
+
 if __name__ == '__main__':
     # Using the project path sets us to the root of the repository.
     use_project_path()
+
+    # Temporary configuration variables will be contained here.  They will hopefully be updated to runtime
+    # arguments once everything is working
+    TRAINING_COLLECTION = 'sample_lg'
+    VALIDATION_SIZE = 0.2
+    BATCH_SIZE = 8
+    INITIALIZE_WITH = 'coco'
+    EPOCHS_HEAD = 1
+    EPOCHS_RESNET = 1
+    EPOCHS_ALL = 1
+
+    MODEL_DIR = 'temp_data/logs'
 
     # This model will make use of the coco weights provided by the mask_rcnn project.
     COCO_MODEL_PATH = 'temp_data/mask_rcnn_coco.h5'
@@ -142,96 +141,42 @@ if __name__ == '__main__':
         print("Downloading Coco Weights...")
         utils.download_trained_weights(COCO_MODEL_PATH)
 
-    exit(0)
+    # Create a pandas array of training images.  Please note that this will perform a default 80/20 training
+    # and testing split.
+    training_images = get_training_images_split(TRAINING_COLLECTION, VALIDATION_SIZE)
+    training_image_count = len(training_images[(training_images['validation'] == 0.0)])
+    validation_image_count = len(training_images[(training_images['validation'] == 1.0)])
 
-    # Results Directory
-    # Save Submission files here
-    RESULTS_DIR = os.path.join(ROOT_DIR, "Results/Buildings/")
-    # Currently a temp set up, just identify valdiation set, contains 171 images from the new formed zoom 20 sample training set
-    VAL_IMAGE_IDS = ['nia_825a50_412_530131_484370_20.png','nia_825a50_413_530132_484370_20.png','nia_825a50_414_530133_484370_20.png','nia_825a50_415_530115_484371_20.png']
-    #################################################
-    # Configurations
-    #################################################
-
-
-
-
-
-
-
+    # Create the mrcnn configuration object.  We are adjusting everything to understand batch size and using
+    # the training and validation lengths as read back in from the pandas dataframe
     config = BuildingConfig()
+    config.STEPS_PER_EPOCH = int(training_image_count / BATCH_SIZE)
+    config.VALIDATION_STEPS = int(validation_image_count / BATCH_SIZE)
     config.display()
 
-    #################################################
-    # Dataset
-    #################################################
-
-
-
-
-
-
-    ############################################
-    # DATA SET LOAD IN
-
-    # Training Dataset
-    dataset_train = BuildingDataset()
-
-    dataset_dir = "D:/Repos/Depaul/DSC672/temp_data/"
-    subset = "sample_lg/"
-    dataset_train.load_building(dataset_dir, subset)
+    # Load the training image dataset.
+    dataset_train = BuildingDataset(training_images)
+    dataset_train.load_building()
     dataset_train.prepare()
 
-    # Print out dataset information regarding the training set
-    print("Image Count: {}".format(len(dataset_train.image_ids)))
-    print("Class Count: {}".format(dataset_train.num_classes))
-    for i, info in enumerate(dataset_train.class_info):
-        print("{:3}. {:50}".format(i, info['name']))
-
-
-
-    # Load validation dataset
-    dataset_val = BuildingDataset()
-    dataset_val.load_building(dataset_dir, "val")
+    # Load validation dataset, done so by setting validation to true
+    dataset_val = BuildingDataset(training_images)
+    dataset_val.load_building(validation=True)
     dataset_val.prepare()
-
-    # Print out Validation dataset info
-    print("Image Count: {}".format(len(dataset_val.image_ids)))
-    print("Class Count: {}".format(dataset_val.num_classes))
-    for i, info in enumerate(dataset_val.class_info):
-        print("{:3}. {:50}".format(i, info['name']))
-
-
-
-    # Load and display random samples
-    #image_ids = np.random.choice(dataset_train.image_ids, 4)
-    #for image_id in image_ids:
-      #  image = dataset_train.load_image(image_id)
-       # mask, class_ids = dataset_train.load_mask(image_id)
-       # visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names, limit= 1) #Coded provided to visualize outputs
-
-    ############################################
-
-
-    ###########################################
-    # TRAINING
 
     # Create model in training mode
     model = modellib.MaskRCNN(mode="training", config=config, model_dir=MODEL_DIR)
 
-    # Which weights to start with?
-    init_with = "coco"  # imagenet, coco, or last
-
-    if init_with == "imagenet":
+    if INITIALIZE_WITH == "imagenet":
         model.load_weights(model.get_imagenet_weights(), by_name=True)
-    elif init_with == "coco":
+    elif INITIALIZE_WITH == "coco":
         # Load weights trained on MS COCO, but skip layers that
         # are different due to the different number of classes
         # See README for instructions to download the COCO weights
         model.load_weights(COCO_MODEL_PATH, by_name=True,
                            exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
                                     "mrcnn_bbox", "mrcnn_mask"])
-    elif init_with == "last":
+    elif INITIALIZE_WITH == "last":
         # Load the last model you trained and continue training
         model.load_weights(model.find_last(), by_name=True)
 
@@ -242,18 +187,19 @@ if __name__ == '__main__':
     # Passing layers="heads" freezes all layers except the head
     # layers. You can also pass a regular expression to select
     # which layers to train by name pattern.
+    print("Training the head branches")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=3,
+                epochs=EPOCHS_HEAD,
                 layers='heads')
 
 
     # Training - Stage 2
     # Finetune layers from ResNet stage 4 and up
-    print("Fine tune Resnet stage 4 and up")
+    print("Training Resnet stage 4 and up")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=10,
+                epochs=(EPOCHS_HEAD + EPOCHS_RESNET),
                 layers='4+')
 
 
@@ -263,9 +209,10 @@ if __name__ == '__main__':
     # Passing layers="all" trains all layers. You can also
     # pass a regular expression to select which layers to
     # train by name pattern.
+    print("Training All Layers")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE / 10,
-                epochs=10,
+                epochs=(EPOCHS_HEAD + EPOCHS_RESNET + EPOCHS_ALL),
                 layers="all")
 
 
@@ -278,25 +225,17 @@ if __name__ == '__main__':
                               config=inference_config,
                               model_dir=MODEL_DIR)
 
-    # /Users/Sebastian/Documents/GitHub/logs/building20200209T1400
-    # Get path to saved weights
-    # Either set a specific path or find last trained weights
-    log_dir = "D:/Repos/DePaul/DSC672/temp_data/logs/building20200213T2133/"
-    model_path = os.path.join(log_dir, "mask_rcnn_building_0005.h5")
-    # model_path = model.find_last()
-
-    # Load trained weights
-    print("Loading weights from ", model_path)
+    # For now, we are just going to find the last weights file.
+    model_path = model.find_last()[1]
     model.load_weights(model_path, by_name=True)
 
+    #
+    # Note for Sebastion: I have no idea how well the rest of this code works...I haven't gotten far enough to
+    # test it out just yet.
+    #
 
     # Test on a random image
-    # Image_ID = 59
-    # Image_ID = 117 street sample
-    # Image Id = 42 Building Sample
-    # Image ID = 44
     image_id = random.choice(dataset_val.image_ids)
-    image_id = 44
 
     print("This is the image_ID", str(image_id)) #Find out which image is showing
     original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
